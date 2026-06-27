9f6a64504beffd3768e15f57ae85e968f60f889e
igor17007 <igor26vinicius@gmail.com>
2026-06-27 13:20:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+let produtos = JSON.parse(localStorage.getItem("produtos")) || [];
+
 function adicionarProduto() {
-    alert("Funcionou!");
+
+    const nome = document.getElementById("nome").value;
+    const categoria = document.getElementById("categoria").value;
+    const quantidade = parseInt(document.getElementById("quantidade").value);
+    const valor = parseFloat(document.getElementById("valor").value);
+
+    if (!nome || !categoria || !quantidade || !valor) {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    const produto = {
+        nome: nome,
+        categoria: categoria,
+        quantidade: quantidade,
+        valor: valor
+    };
+
+    produtos.push(produto);
+
+    localStorage.setItem("produtos", JSON.stringify(produtos));
+
+    alert("Produto salvo com sucesso!");
+
+    document.getElementById("nome").value = "";
+    document.getElementById("quantidade").value = "";
+    document.getElementById("valor").value = "";
 }
